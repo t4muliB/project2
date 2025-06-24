1e7d4af801462e235457928cf3d880e768f0c1a8
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2025-06-24 21:00:57 +0530

    files

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'host' => '127.0.0.1',
+    'port' => '3306',
+    'dbname' => 'my_db',
+    'charset' => 'utf8mb4'
+];
---
+++ routes.php
@@ -0,0 +1,7 @@
+<?php
+
+$router->get('/', 'controllers/home.php');
+$router->get('/upload', 'controllers/upload.php');
+$router->get('/login', 'controllers/login.php');
+$router->get('/users', 'controllers/users.php');
+$router->get('/db', 'controllers/db.php');
