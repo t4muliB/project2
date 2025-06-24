ad1911f29c4499417a7ed287822a9fef27ff3e4e
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2025-06-24 21:03:25 +0530

    modified

--- index.php
+++ index.php
@@ -1,23 +1,90 @@
 <?php
 
-
-
-// define('STORAGE_PATH', __DIR__ . '/../storage');
 define('STORAGE_PATH', __DIR__ . '/storage');
 
 require "functions.php";
 
-$uri = $_SERVER['REQUEST_URI'];
+spl_autoload_register(function($class) {
+    require($class . '.php');
+});
 
-if($uri === '/') {
-    require 'controllers/home.php';
-} else if($uri === '/upload') {
-    require 'controllers/upload.php';
-}
+$router = new Router();
+
+$routes = require "routes.php";
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
 
 
-// $routes = require("routes.php");
 
-// $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-// $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if($uri === '/') {
+//     require 'controllers/home.php';
+// } else if($uri === '/upload') {
+//     require 'controllers/upload.php';
+// } else if($uri === '/login') {
+//     require 'controllers/login.php';
+// } else if(str_starts_with($uri,'/users')) { 
+// require 'controllers/users.php';
+// }
